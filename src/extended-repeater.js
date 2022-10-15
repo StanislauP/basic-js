94d7dc7d490c17d138693ef86e0375cb246cf57e
StanislauP <104429072+StanislauP@users.noreply.github.com>
2022-10-15 18:35:40 +0300

    Add files via upload

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options ) {
+  let a = ""
+  let arr = []
+  let res =[] 
+  let db = {separator:"+", additionSeparator:"|", repeatTimes:1, additionRepeatTimes:1, addition:""}
+  for(key in db){if(options[key] === undefined){options[key]=db[key]}}
+  a = str;
+  for (let j = 0; j < options.additionRepeatTimes; j++){
+    arr.push(String(options.addition))}
+  a += arr.join(`${options.additionSeparator}`)
+  for (let l = 0; l < options.repeatTimes; l++){
+    res.push(a)}
+  return res.join(`${options.separator}`);
+  };
 
 module.exports = {
   repeater
