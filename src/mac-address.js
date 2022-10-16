6bc5c63afe8ed95dbf99d7ba7c6d141c2cbafceb
StanislauP <104429072+StanislauP@users.noreply.github.com>
2022-10-16 17:06:20 +0300

    Add files via upload

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function isMAC48Address( n) {
+  // throw new NotImplementedError('Not implemented');
+   // remove line with error and write your code here
+   if(n.length!=17 || n[0]==='Z' || n[0]==='G'){return false}
+   return true
+ }
 module.exports = {
   isMAC48Address
 };
